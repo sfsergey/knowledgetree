bf4e1670fbf1a58d5f3024049c511a14f88c2bfd
megan_w <megan_w@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-05 13:42:52 +0000

    Fixed bugs in export and import
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/branches/3.6.x-megan-Branch@8574 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/doctrine/ModelManager.php
+++ sql/doctrine/ModelManager.php
@@ -1,19 +1,33 @@
 <?php
 
-define('KT_ROOT_DIR', realpath(__FILE__ . '../../') . DIRECTORY_SEPARATOR);
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . KT_ROOT_DIR.'/thirdparty/Doctrine');
-
-define('KT_DOCTRINE_BASE_DIR', KT_ROOT_DIR.'/sql/doctrine/base');
-
-require_once('lib/Doctrine.php');
-spl_autoload_register(array('Doctrine', 'autoload'));
+/**
+ * Manager class for managing the import and export of the database tables and base classes
+ */
 
 class ModelManager
 {
     /**
+     * Initialise the Model Manager
+     */
+    public static
+    function initManager()
+    {
+        $modelPath = dirname(__FILE__) . '/../..';
+        define('KT_ROOT_DIR', realpath($modelPath) . DIRECTORY_SEPARATOR);
+
+        define('KT_DOCTRINE_BASE_DIR', KT_ROOT_DIR.'sql/doctrine/base/');
+        define('KT_DOCTRINE_TMP_DIR', KT_ROOT_DIR.'sql/doctrine/tmp/');
+
+        ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . KT_ROOT_DIR.'thirdparty/Doctrine');
+
+        require_once('lib/Doctrine.php');
+        spl_autoload_register(array('Doctrine', 'autoload'));
+    }
+
+    /**
      * Establish connection to database using doctrine
      *
-     * @param unknown_type $dsn
+     * @param string $dsn
      * @return unknown
      */
     static private
@@ -27,25 +41,32 @@ class ModelManager
     /**
      * Fetch the database connection
      *
-     * @param unknown_type $db
-     * @return unknown
+     * @param Doctrine_Connection $db
+     * @return Doctrine_Connection
      */
     static public
     function &getDB($db = null) {
-        global $default;
+        static $conn = null;
 
-        if(!is_null($db)){
-            $default->_db = $db;
+        if(!is_null($db) && $db instanceof Doctrine_Connection){
+            $conn = $db;
         }
 
-        if (is_null($default->_db)) {
-            $db = DBUtil::connect("mysql://root:root@localhost/ktdms_kt36");
-            $default->_db = $db;
+        if (is_null($conn)) {
+            $dsn = 'mysql://root:root@localhost/kt_refactor';
+            $db = Doctrine_Manager::connection($dsn);
+            $conn = $db;
         }else{
-            $db = $default->_db;
+            $db = $conn;
         }
         return $db;
     }
+
+    /**
+     * Create base classes from the database tables.
+     *
+     * @param string $dir The directory in which to save the tables
+     */
     static public
     function exportTables($dir = null)
     {
@@ -60,15 +81,78 @@ class ModelManager
             }
         }
 
-        $db = DoctrineUtil::getDB();
-        Doctrine::generateModelsFromDb($dir);
+        $tmp = KT_DOCTRINE_TMP_DIR;
+
+        if(!file_exists($tmp)){
+            mkdir($tmp, 0755);
+            if(!file_exists($tmp)){
+                throw new Exception('Cannot create temporary export directory for models '.$tmp);
+            }
+        }
+
+        // Generate models and base classes
+        $db = ModelManager::getDB();
+        $manager = Doctrine_Manager::getInstance();
+        $manager->setAttribute(Doctrine::ATTR_EXPORT, Doctrine::EXPORT_CONSTRAINTS); // Doctrine::EXPORT_TABLES | Doctrine::EXPORT_CONSTRAINTS
+
+        // Set the template
+//        $tpl = <<<END
+//%sclass %s extends %s
+//{
+//%s
+//%s
+//%s
+//}
+//END;
+//
+//        $options = array(
+//                'BaseClassPrefix' => 'Base_',
+//                'generateTableClasses' => false,
+//                'BaseClassesDirectory' => 'Base',
+//                '_tpl' => $tpl
+//            );
+
+        $options = array('detect_relations' => true);
+        Doctrine::generateModelsFromDb($tmp, array(), $options);
+
+
+        // Recurse through the base classes
+        // Change the class name to match the directory structure ie BaseClass becomes Base_Class
+        // Save the class to the given directory and rename the file to Class.php.
+        $tmpGenerated = $tmp . 'generated' . DIRECTORY_SEPARATOR;
+        $dirh = opendir($tmpGenerated);
+        while (($entry = readdir($dirh)) !== false) {
+            if (in_array($entry, array('.', '..'))) {
+                continue;
+            }
+            $newpath = $tmpGenerated . $entry;
+            if (is_dir($newpath) || !is_file($newpath)) {
+                continue;
+            }
+
+            $class = substr($entry, 4, -4);
+            $newFile = $dir . $class . '.php';
+
+            // Rename the class from BaseClass to Base_Class and remove the abstract
+            $fileData = file_get_contents($newpath);
+            $fileData = str_replace('abstract class Base'.$class, 'class Base_'.$class, $fileData);
+            file_put_contents($newFile, $fileData);
+        }
+
+        // Delete temporary files
+        self::deleteDir($tmp);
     }
 
+    /**
+     * Create database tables from the base class definitions.
+     *
+     * @param string $dir The directory in which the classes are stored.
+     */
     static public
     function importTables($dir = null)
     {
         if(is_null($dir)){
-            $dir = KT_DOCTRINE_BASE_DIR . DIRECTORY_SEPARATOR . 'generated';
+            $dir = KT_DOCTRINE_BASE_DIR;
         }
 
         if(!file_exists($dir)){
@@ -76,12 +160,47 @@ class ModelManager
         }
 
         $db = DoctrineUtil::getDB();
-        $manager->setAttribute(Doctrine::ATTR_EXPORT, Doctrine::EXPORT_ALL); // Doctrine::EXPORT_TABLES | Doctrine::EXPORT_CONSTRAINTS
-
         Doctrine::createTablesFromModels($dir);
+    }
+
+    /**
+     * Recursively delete a directory
+     *
+     * @param string $dir
+     */
+    static private
+    function deleteDir($dir)
+    {
+        if(substr(PHP_OS, 0, 3) == 'WIN') {
+            // Potentially kills off all the files in the path, speeding
+            // things up a bit
+            exec("del /q /s " . escapeshellarg($dir));
+        } else {
+            if (file_exists('/bin/rm')) {
+                exec("/bin/rm -rf $dir");
+                return;
+            }
+        }
+
+        $hPath = @opendir($dir);
+        while (($sFilename = readdir($hPath)) !== false) {
+            if (in_array($sFilename, array('.', '..'))) {
+                continue;
+            }
+            $sFullFilename = sprintf("%s/%s", $sPath, $sFilename);
+            if (is_dir($sFullFilename)) {
+                self::deleteDir($sFullFilename);
+                continue;
+            }
+            @chmod($sFullFilename, 0666);
+            @unlink($sFullFilename);
+        }
+        closedir($hPath);
+        @rmdir($dir);
     }
 }
 
-DoctrineUtil::
+ModelManager::initManager();
+ModelManager::exportTables();
 
 ?>
